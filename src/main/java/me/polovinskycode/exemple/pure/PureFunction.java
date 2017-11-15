1a7ae3b08c73c9a9bd23353263679a9ae71b6229
Diego Rocha <diegopolovinsky@gmail.com>
2017-11-15 18:03:55 -0200

    First commit

---
+++ src/main/java/me/polovinskycode/exemple/pure/PureFunction.java
@@ -0,0 +1,8 @@
+package me.polovinskycode.exemple.pure;
+
+public class PureFunction {
+
+    public Double somar(Double x, Double y) {
+        return x + y;
+    }
+}
---
+++ src/test/java/me/polovinskycode/exemple/pure/PureFunctionTest.java
@@ -0,0 +1,24 @@
+package me.polovinskycode.exemple.pure;
+
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PureFunctionTest {
+
+    private PureFunction pure;
+
+    @Before
+    public void setUp() throws Exception {
+        pure = new PureFunction();
+    }
+
+    @Test
+    public void soma_comValorA_comValorB_deveRetonarSomaDeDosDoisValores() throws Exception {
+        Double x = 5d;
+        Double y = 5d;
+        Double resultado = pure.somar(x, y);
+        Assert.assertThat(resultado, Matchers.equalTo(10d));
+    }
+}
